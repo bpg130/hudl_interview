1ea5aa2b0baee0a3d1e2b90d7e3fbf858382cf70
Ben <benpage@umich.edu>
2015-10-07 12:05:34 -0400

    moved folder around so this is an alias now

---
+++ getAthletes.php
@@ -0,0 +1,5 @@
+hi' OR '1'='1
+username=victim&password=hi%27%20OR%20%271%27%3D%
+hi%27 OR %271%27=%271
+
+'select pw from users where username='victim'
--- getSchools.php
+++ getSchools.php
@@ -1,1 +1,33 @@
-/Applications/XAMPP/htdocs/hudl_interview/getSchools.php
+<?php
+
+$mysqli = new mysqli('localhost','root','','hudl_interview');
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+else {
+
+	$res = $mysqli->query("Select * from schools");
+	$data = array();
+	while( $row = $res->fetch_assoc()){
+
+		$school_name = $row["school_name"];
+
+		$data[] = ["school_name"=> $row["school_name"], "lat"=>$row["lat"], "lng"=>$row["lng"]];
+
+	}
+	foreach ($data as &$school){
+		$res = $mysqli->query("select * from players where school_name = '" . $school["school_name"]."'");
+		$players = array();
+		while ($row_1 = $res->fetch_assoc()){
+			
+			$players[] = ["player_name"=>$row_1["player_name"], "grad_year"=>$row_1["grad_year"], "player_link"=>$row_1["player_link"]];
+		
+		}
+		$school["players"] = $players;
+	}
+	
+	echo json_encode($data);
+}
+
+?>
